092bd694ed7458ac106300bdf1656eb098a361ac
ReBiliBin <qq2657519792@163.com>
2024-12-15 17:21:10 +0800

    New Version of BinBot

--- feature.py
+++ feature.py
@@ -1,17 +1,23 @@
 import random
-from types import Callable
+import processer
+from typing import Callable
 from typing import Any
-key: str = open("key.txt").read()
-def send_msg(text:str)
-    "操你妈不会写"
-    raise NotImplementedError
+from botpy import logging
+
+_log = logging.get_logger()
+
 def record(func:Callable,*args:Any):
     "记录器"
     #open()
     try:
         return func(*args)
     except Exception as e:
-        send_msg(f"错误{e}")
+        return(f"错误：{e}")
+@record
+def ture_misuc_random_select():
+    "神曲选择器"
+    song="Speculation (BV1834y1M7Sr)","THE CANNONBALLER (BV1BW411b7mr)"
+    return random.choice(song)
 @record
 def random_num(min:int,max:int):
     "哈人随机"
@@ -19,9 +25,4 @@ def random_num(min:int,max:int):
         if min<8000 and max >9000:
             return random.randint(min,max) if round(random.uniform(0.0,1.0),2) != 0.24 else 9000 - 17*2
         return random.randint(min,max)
-    raise SyntaxError
-@record
-def ture_misuc_random_select():
-    "神曲选择器"
-    song="Speculation (https://b23.tv/lGN2FVp)","THE CANNONBALLER (https://b23.tv/HJNhBTV)"
-    return random.choice(song)
+    raise SyntaxError("The Min is Bigger than Max.")
